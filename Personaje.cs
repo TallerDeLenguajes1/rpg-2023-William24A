f6a178a1b9c605c6189d88fa1bc02efdd7fc3b15
William24A <willi241194@gmail.com>
2023-06-15 18:24:58 -0300

    clase crear personaje creada caracteristicas

--- Personaje.cs
+++ Personaje.cs
@@ -26,6 +26,10 @@ public class Personaje{
         this.Salud = salud;
     }
 
+    public Personaje(){
+
+    }
+
     public string Tipo { get => tipo; set => tipo = value; }
     public string Nombre { get => nombre; set => nombre = value; }
     public string Apodo { get => apodo; set => apodo = value; }
@@ -36,4 +40,56 @@ public class Personaje{
     public int Nivel { get => nivel; set => nivel = value; }
     public int Armadura { get => armadura; set => armadura = value; }
     public int Salud { get => salud; set => salud = value; }
+}
+
+public class FabricaDePersonaje{
+    public Personaje CrearPersonaje(){
+        var personajeAleatorio = new Personaje();
+        personajeAleatorio.Nombre = ObtenerNombre();
+        personajeAleatorio.Tipo = ObtenerTipo();
+        personajeAleatorio.Apodo = ObtenerApodo();
+        personajeAleatorio.Fechanacimiento = DateTime.Now;
+        personajeAleatorio.Velocidad = ObtenerIntRandom(1,10);
+        personajeAleatorio.Destreza =ObtenerIntRandom(1,5);
+        personajeAleatorio.Fuerza = ObtenerIntRandom(1,10);
+        personajeAleatorio.Nivel= 1;
+        personajeAleatorio.Armadura = ObtenerIntRandom(1,10);
+        personajeAleatorio.Salud = 100;
+
+        return personajeAleatorio;
+    }
+
+    public int ObtenerIntRandom(int ini,int  fin){
+        var random = new Random();
+        return random.Next(ini,fin);
+    }
+
+    public string ObtenerNombre(){
+        var nombres = new string[3];
+        nombres[0] = "Raul";
+        nombres[1] = "Mauro";
+        nombres[2] = "Messi";
+        var elegir = ObtenerIntRandom(0,3);
+        return nombres[elegir];
+    }
+
+    public string ObtenerTipo(){
+        var tipo = new string[3];
+        tipo[0] = "Humano";
+        tipo[1] = "Orco";
+        tipo[2] = "Elfo";
+        
+        var elegir = ObtenerIntRandom(0,3);
+        return tipo[elegir];
+    }
+
+    public string ObtenerApodo(){
+        var apodo = new string[3];
+        apodo[0] = "El tronco";
+        apodo[1] = "Pancho";
+        apodo[2] = "Dios";
+
+        var elegir = ObtenerIntRandom(0,3);
+        return apodo[elegir];
+    }
 }
